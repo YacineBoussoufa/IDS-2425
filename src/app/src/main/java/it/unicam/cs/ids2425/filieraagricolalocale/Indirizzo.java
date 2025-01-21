3d1849d2590e603535fe737f155d36bcb092280e
camillo-yyy <camillohd@gmail.com>
2025-01-21 01:41:36 +0100

    Aggiornamento modello
    Creazione classi Utente, persona, ruolo, carrello, ordine, linea ordine.

---
+++ src/app/src/main/java/it/unicam/cs/ids2425/filieraagricolalocale/Indirizzo.java
@@ -0,0 +1,80 @@
+package it.unicam.cs.ids2425.filieraagricolalocale;
+
+public class Indirizzo {
+
+   private String via;
+   private int civico;
+   private String provincia;
+   private String comune;
+   private String regione;
+   private String CAP;
+   private String dettagli;
+   
+   public Indirizzo(String via, int civico, String provincia, String comune, String regione, String cAP,
+         String dettagli) {
+      this.via = via;
+      this.civico = civico;
+      this.provincia = provincia;
+      this.comune = comune;
+      this.regione = regione;
+      CAP = cAP;
+      this.dettagli = dettagli;
+   }
+
+   public String getVia() {
+      return via;
+   }
+
+   public void setVia(String via) {
+      this.via = via;
+   }
+
+   public int getCivico() {
+      return civico;
+   }
+
+   public void setCivico(int civico) {
+      this.civico = civico;
+   }
+
+   public String getProvincia() {
+      return provincia;
+   }
+   
+   public void setProvincia(String provincia) {
+      this.provincia = provincia;
+   }
+
+   public String getComune() {
+      return comune;
+   }
+   
+   public void setComune(String comune) {
+      this.comune = comune;
+   }
+
+   public String getRegione() {
+      return regione;
+   }
+
+   public void setRegione(String regione) {
+      this.regione = regione;
+   }
+
+   public String getCAP() {
+      return CAP;
+   }
+
+   public void setCAP(String cAP) {
+      CAP = cAP;
+   }
+
+   public String getDettagli() {
+      return dettagli;
+   }
+   
+   public void setDettagli(String dettagli) {
+      this.dettagli = dettagli;
+   }
+
+}
---
+++ src/app/src/main/java/it/unicam/cs/ids2425/filieraagricolalocale/Ruolo.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.ids2425.filieraagricolalocale;
+
+import java.util.List;
+
+public interface Ruolo {
+
+   List<String> getPermessi();
+
+}
