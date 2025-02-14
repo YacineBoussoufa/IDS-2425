2162ba9e233bf38d202746d71bba0ffa3a2018a5
giacomopess <giacomo020803@gmail.com>
2025-02-14 19:15:24 +0100

    Piccola modifica

--- src/FilieraAgricolaLocale/src/main/java/it/unicam/cs/ids2425/filieraagricolalocale/services/ApprovazioneService.java
+++ src/FilieraAgricolaLocale/src/main/java/it/unicam/cs/ids2425/filieraagricolalocale/services/ApprovazioneService.java
@@ -1,10 +1,7 @@
 package it.unicam.cs.ids2425.filieraagricolalocale.services;
 
 import it.unicam.cs.ids2425.filieraagricolalocale.exceptions.DatiIncorrettiException;
-import it.unicam.cs.ids2425.filieraagricolalocale.model.Bozza;
-import it.unicam.cs.ids2425.filieraagricolalocale.model.InConvalida;
-import it.unicam.cs.ids2425.filieraagricolalocale.model.Prodotto;
-import it.unicam.cs.ids2425.filieraagricolalocale.model.StatoApprovazione;
+import it.unicam.cs.ids2425.filieraagricolalocale.model.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,7 +19,7 @@ public class ApprovazioneService {
      * @param id Identificatore del prodotto che si vuole far convalidare
      */
     public void inviaRichiesta(int id) {
-        Prodotto prodotto = repoProdotti.get(id);
+        Contenuto prodotto = repoProdotti.get(id);
 
         if (prodotto.getStato() instanceof Bozza) {
             prodotto.getStato().pubblica();
@@ -34,10 +31,10 @@ public class ApprovazioneService {
      *
      * @return Lista dei prodotti con lo stato InConvalida
      */
-    public List<Prodotto> getProdottiInConvalida() {
-        List<Prodotto> prodotti = new ArrayList<>();
+    public List<Contenuto> getProdottiInConvalida() {
+        List<Contenuto> prodotti = new ArrayList<>();
 
-        for (Prodotto prodotto : repoProdotti.values()) {
+        for (Contenuto prodotto : repoProdotti.values()) {
             if (prodotto.getStato() instanceof InConvalida) {
                 prodotti.add(prodotto);
             }
@@ -55,7 +52,7 @@ public class ApprovazioneService {
      * @throws DatiIncorrettiException se il prodotto non è pronto alla convalida
      */
     public void approva(int id, boolean esito) {
-        Prodotto prodotto = repoProdotti.get(id);
+        Contenuto prodotto = repoProdotti.get(id);
         StatoApprovazione stato = prodotto.getStato();
 
         if (!(stato instanceof InConvalida)) throw new DatiIncorrettiException();
